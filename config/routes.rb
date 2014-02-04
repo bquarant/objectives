b15c2f6fa5177ad53848c4c5128ca6ec0f893e30
Brian Quaranto <quaranto@gmail.com>
2014-02-03 23:57:41 -0500

    make a devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Objectives::Application.routes.draw do
+  devise_for :users
   resources :responses
 
   resources :exams
@@ -12,7 +13,7 @@ Objectives::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'about#index'
+  root :to => "about#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
